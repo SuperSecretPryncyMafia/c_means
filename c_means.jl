34bc4ece7c82836ecc6bbb81712b555a1eeeb138
ArturOle <artur.oleksinski99@gmail.com>
2021-12-21 16:32:42 +0100

    TDD, cluster centers fix, distances fix, everything fix

--- c_means.jl
+++ c_means.jl
@@ -8,25 +8,23 @@ data =  [2.5 3 3 3.5 5.5 6 6 6.5; 3.5 3 4 3.5 5.5 6 5 5.5]
 println(data)
 
 function cluster_centers(partition_table, data)  #  prototype
-    s = size(partition_table)
-    number_of_rows = s[1]
-    entries = s[2]
-    V_row = []
-    v = []    
+    
+    size_of_data = size(data)
+    v = zeros(size_of_data[1], size_of_data[1])   
 
-    for row_number in 1:number_of_rows
-    
-        summed_nominator = 0
-        summed_denominator = 0
-        for entry_number in 1:entries
-            summed_denominator += partition_table[row_number, entry_number]
-            summed_nominator += data[row_number, entry_number]*partition_table[row_number, entry_number]
-        end
-        append!(V_row, [summed_nominator/summed_denominator])
-    
-        append!(v, [V_row])
+    for partition_table_row in 1:size_of_data[1]
+        for data_row in 1:size_of_data[1]
+            summed_nominator = 0
+            summed_denominator = 0
+            for entry_number in 1:size_of_data[2]
+                summed_denominator += partition_table[partition_table_row, entry_number]
+                summed_nominator += data[data_row, entry_number]*partition_table[partition_table_row, entry_number]
+            end
+            v[partition_table_row, data_row] = summed_nominator/summed_denominator
+        end     
     end
-    println(v)
+    display(v) 
+    return reverse(rotr90(v))
 end
 
 # prototype(patition_table) = v(partition_table)
@@ -40,36 +38,73 @@ function lagrange_multipliers()
 end
 
 function c_means(data, number_of_clusters)
+    J = [Inf]
+    ϵ = 5
     Uₕ = zeros(Int, number_of_clusters, size(data,2))
     show_partition_matrix(Uₕ)
     Uₕ = init_random(Uₕ)
     show_partition_matrix(Uₕ)
     Vₕ = cluster_centers(Uₕ, data)
-
-
-end
-
-function euclidian_distances(v1::Vector, v2::Vector)
-    if size(v1) == size(v2)
-        s = size(v1,1)
-        d = zeros((length(v1[1]), s))
-        
-        for j in 1:length(v2[1])
-            for k in 1:length(v1[1])
-                summed = 0
-                for i in 1:s
-                    # println(v1[1], " ", v1[2], " ", v2[1], " ", v2[2])
-                    summed += (v1[i][k] - v2[i][j])^2
-                end
-                d[k, j] = summed
-            end
-        end
-        # println(length(v1[1]))
-        return d
+    d = euclidian_distances(data, Vₕ)
+    println(d)
+    append!(J, [criterion_function(Uₕ, d)])
+    println(J)
+    while minimum(J) > ϵ
+        Uₕ = zeros(Int, number_of_clusters, size(data,2))
+        show_partition_matrix(Uₕ)
+        Uₕ = init_random(Uₕ)
+        show_partition_matrix(Uₕ)
+        Vₕ = cluster_centers(Uₕ, data)
+        d = euclidian_distances(data, Vₕ)
+        println(d)
+        append!(J, [criterion_function(Uₕ, d)])
+        println(J)
     end
+    println(Uₕ, "\n", Vₕ, "\n", d)
 end
 
-function update_partition_table(n, data, prototypes)
+function euclidian_distances(v1::Matrix{Float64}, v2::Matrix{Float64})
+    v1 = [v1[i, :] for i in 1:size(v1,1)]
+    v2 = [v2[i, :] for i in 1:size(v2,1)]
+    s = size(v1,1)
+    d = zeros((length(v1[1]),s,))
+    
+    for j in 1:length(v2[2])
+        for k in 1:length(v1[1])
+            summed = 0
+            for i in 1:s
+                # println(v1[1], " ", v1[2], " ", v2[1], " ", v2[2])
+                summed += (v1[i][k] - v2[i][j])^2
+            end
+            d[ k, j] = summed
+        end
+    end
+    # println(length(v1[1]))
+    display(d)
+    return d'
+
+end
+
+function euclidian_distances(v1::Vector, v2)
+    s = size(v1,1)
+    d = zeros((length(v1[1]), s))
+    
+    for j in 1:length(v2[1])
+        for k in 1:length(v1[1])
+            summed = 0
+            for i in 1:s
+                # println(v1[1], " ", v1[2], " ", v2[1], " ", v2[2])
+                summed += (v1[i][k] - v2[i][j])^2
+            end
+            d[k, j] = summed
+        end
+    end
+    # println(length(v1[1]))
+    display(d)
+    return d'
+end
+
+function update_partition_table(partition_table, distances)
     partition_table = zeros(Int, n, size(data,2))
     for k in 1:size(partition_table, 1)
         for i in 1:size(partition_table, 2)
@@ -78,8 +113,14 @@ function update_partition_table(n, data, prototypes)
     end
 end
 
-function criterion_function(partition_table)
-
+function criterion_function(partition_table, distances)
+    J = 0
+    for column in 1:size(partition_table,2)
+        for row in 1:size(partition_table,1)
+            J += partition_table[row, column]*distances[row, column]
+        end
+    end
+    return J
 end
 
 function init_random(partition_table)
@@ -106,14 +147,41 @@ function show_partition_matrix(partition_matrix)
     print(join([join((" x", i)) for i in 1:length(eachcol(partition_matrix))]), "\n")
 end
 
-c_means(data, 2)
+#c_means(data,2)
 # scatter(data[1,:], data[2,:])
 
 function euclidian_distances_test()
-    v1 = [[2.5,3,3,3.5,5.5,6,6,6.5],[3.5,3,4,3.5,5.5,6,5,5.5]]
-    v2 = [[4.25,4.75],[4.25, 4.75]]
+    v1 = [2.5 3 3 3.5 5.5 6 6 6.5;3.5 3 4 3.5 5.5 6 5 5.5]
+    v2 = [4.25 4.75;4.25 4.75]
     result = euclidian_distances(v1, v2)
+
     display(result)
 
 end
-# euclidian_distances_test()
+
+function test_cluster_centers()
+    v1 = [2.5 3 3 3.5 5.5 6 6 6.5;3.5 3 4 3.5 5.5 6 5 5.5]
+    pt = [0 0 0 0 1 1 1 1 ;1 1 1 1 0 0 0 0]
+    result = cluster_centers(pt, v1)
+    display(result)
+end
+
+function test_criterion_function()
+    J = [Inf]
+    data = [2.5 3 3 3.5 5.5 6 6 6.5;3.5 3 4 3.5 5.5 6 5 5.5]
+    Uₕ = [1 1 0 0 1 0 1 0; 0 0 1 1 0 1 0 1]
+    show_partition_matrix(Uₕ)
+    # Uₕ = init_random(Uₕ)
+    # show_partition_matrix(Uₕ)
+    Vₕ = cluster_centers(Uₕ, data)
+    display(Vₕ)
+    d = euclidian_distances(data, Vₕ)
+    display(d)
+    append!(J, [criterion_function(Uₕ, d)])
+    println(J)
+end
+
+
+euclidian_distances_test()
+test_cluster_centers()
+test_criterion_function()
